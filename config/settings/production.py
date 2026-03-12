92a8b861a4a9b8ee6b6cecce53f70529cce107a4
Maksim Kuzmin <avatarmakc@yandex.ru>
2026-03-12 10:12:58 +0900

    Production marks.

--- config/settings/production.py
+++ config/settings/production.py
@@ -3,17 +3,25 @@ from .base import *  # noqa: F401,F403
 DEBUG = False
 
 SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
-SESSION_COOKIE_SECURE = True
-CSRF_COOKIE_SECURE = True
-SECURE_HSTS_SECONDS = 31536000
-SECURE_HSTS_INCLUDE_SUBDOMAINS = True
-SECURE_HSTS_PRELOAD = True
+
+# пока нет HTTPS
+SESSION_COOKIE_SECURE = False
+CSRF_COOKIE_SECURE = False
+
+SECURE_HSTS_SECONDS = 0
+SECURE_HSTS_INCLUDE_SUBDOMAINS = False
+SECURE_HSTS_PRELOAD = False
+
 SECURE_BROWSER_XSS_FILTER = True
 SECURE_CONTENT_TYPE_NOSNIFF = True
-SECURE_SSL_REDIRECT = env_bool("SECURE_SSL_REDIRECT", True)
 
-# WhiteNoise enables static serving in simple Gunicorn deployments.
+SECURE_SSL_REDIRECT = env_bool("SECURE_SSL_REDIRECT", False)
+
+USE_X_FORWARDED_HOST = True
+
+# WhiteNoise
 MIDDLEWARE = ["whitenoise.middleware.WhiteNoiseMiddleware", *MIDDLEWARE]
+
 STORAGES = {
     "default": {"BACKEND": "django.core.files.storage.FileSystemStorage"},
     "staticfiles": {"BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage"},
