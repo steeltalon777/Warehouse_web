1bc136e2873326324547d5f9aec017b8c30735cd
makc <makc@warehouse-solution.dev>
2026-09-17 13:08:03 +0900

    chore(documents): promote waybill template to 2.2.3

--- apps/documents/tests/test_envelope_builder.py
+++ apps/documents/tests/test_envelope_builder.py
@@ -13,7 +13,7 @@ from apps.documents.services import (
 
 DEFAULT_DOCUMENT_TYPE = "waybill"
 MAPPED_TEMPLATE_ID = "warehouse-waybill-ru"
-MAPPED_TEMPLATE_VERSION = "2.2.2"
+MAPPED_TEMPLATE_VERSION = "2.2.3"
 
 
 _MISSING = object()
